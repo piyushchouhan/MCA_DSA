5190e147396801940c90fe4f9ab94e69ed6cb092
Piyush Chauhan <piyush9695@gmail.com>
2022-12-25 15:55:30 +0530

    code added

--- Assignment/Fibonacci2.c
+++ Assignment/Fibonacci2.c
@@ -1,2 +1,23 @@
 //PROGRAM TO FIND Nth TERM OF FIBONACCI SERIES (TAIL RECURSIVE).
 
+#include <stdio.h>
+#include <math.h>
+
+int FibonacciTailRecursion(int n, int x , int y)
+{
+    if (n == 1)
+    {
+        return x;
+    }
+    return FibonacciTailRecursion(n-1,y,x+y);
+}
+
+int main()
+{
+    int n,Value;
+    printf("Eneter the nth no. you want to print: ");
+    scanf("%d", &n);
+    Value = FibonacciTailRecursion(n,0,1);
+    printf("The number at index %d in the Fibonacci sequence is %d. \n", n, Value);
+    return 0;
+}
