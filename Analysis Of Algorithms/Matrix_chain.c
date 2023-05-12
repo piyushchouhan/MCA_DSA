2898cd1ae42c9668ca51b5a1512c23a77ac570fc
Piyush Chauhan <piyush9695@gmail.com>
2023-05-12 18:37:01 +0530

    code changed

--- Analysis Of Algorithms/Matrix_chain.c
+++ Analysis Of Algorithms/Matrix_chain.c
@@ -11,9 +11,9 @@ void matrixChainOrder(int p[], int n, int m[][8], int s[][8]) {
         m[i][i] = 0;
     }
 
-    for (int L = 2; L <= n; L++) {
-        for (int i = 1; i <= n - L + 1; i++) {
-            int j = i + L - 1;
+    for (int l = 2; l <= n; l++) {
+        for (int i = 1; i <= n - l + 1; i++) {
+            int j = i + l - 1;
             m[i][j] = INT_MAX;
             for (int k = i; k <= j - 1; k++) {
                 int q = m[i][k] + m[k+1][j] + p[i-1]*p[k]*p[j];
